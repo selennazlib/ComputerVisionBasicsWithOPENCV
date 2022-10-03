f692139cf6312ed6f59c59cf9a0f0a663c0c4043
SelenNB <79100203+selennazlib@users.noreply.github.com>
2022-10-03 11:49:45 +0300

    Eye detection project modified -> + eye detection on webcam

--- EyeDetection/EyeDetection.py
+++ EyeDetection/EyeDetection.py
@@ -35,3 +35,37 @@ cv2.imshow('Image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+
+# In[22]:
+
+
+# WEBCAM
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    2
+    faces = face_cascade.detectMultiScale(gray_frame, 1.3, 5)
+    
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        
+    roi_frame = frame[y: y + h, x: x + w]
+    roi_gray = gray_frame[y: y + h, x: x + w]
+    
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    
+    for (ex, ey, ew, eh) in eyes:
+        cv2.rectangle(roi_frame, (ex, ey), (ex + ew, ey + eh), (255, 25, 160), 2)
+    
+    cv2.imshow('Frame', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
